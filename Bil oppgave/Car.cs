22a21e2aca21e0be0746e4828147923b7d26619c
Kris10anG <kristiang@getacademy.no>
2022-03-13 21:22:23 +0100

    Add project files.

---
+++ Bil oppgave/Car.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Bil_oppgave
+{
+    public class Car
+    {
+        public int Distance = 10000;
+        public int HalfDistance = 5000;
+        public int TopSpeed = 10;
+        public Random RandomSpeed = new Random();
+        public int distanceDriven = 0;
+        public string RegNr { get; set; }
+
+        public Car(string regnr)
+        {
+            RegNr = regnr;
+        }
+
+     
+
+        public void DriveCar()
+        {
+            //var distanceDriven = 0;
+            //for (int TopSpeed = 10; TopSpeed < Distance; TopSpeed += 10)
+            //    while (distanceDriven <= Distance)
+            //    {
+                    TopSpeed += RandomSpeed.Next(60, 201);
+                distanceDriven += TopSpeed;
+                //CarSlowDown(distanceDriven);
+                Console.WriteLine(RegNr + " Hastigheten er " + TopSpeed);
+                Console.WriteLine(RegNr + " distansen kjørt er " + distanceDriven);
+                PrintFinish(distanceDriven);
+            //}
+        }
+
+        //public void CarSlowDown(int distanceDriven)
+        //{
+        //    if (distanceDriven >= HalfDistance)
+        //    {
+        //        if (TopSpeed >= 30)
+        //        {
+        //            TopSpeed -= 20;
+        //        }
+        //    }
+        //}
+
+        public void PrintFinish(int distanceDriven)
+        {
+            if (distanceDriven == Distance)
+            {
+                Console.WriteLine("Løpet er ferdig");
+            }
+        }
+    }
+}
---
+++ Bil oppgave/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Bil_oppgave
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            var car = new Car("LJ123");
+            var car2 = new Car("LA444");
+
+            int Distance = 10000;
+
+            while (car.distanceDriven < Distance && car2.distanceDriven < Distance)
+            {
+                car.DriveCar();
+                car2.DriveCar();
+            }
+            //Du skal lage en konsollapp med en bil som skal kjøre 1000m.Bilen har en start - hastighet på 10m / s(en iterasjon i en løkke er et sekund)
+            //.Bilen skal øke farten med 10m / s frem til den har kjørt 500m, også skal den senke farten med 10m /
+            //s igjen frem til den når minimumshastigheten sin på 10m / s.Når bilen har kommet seg til slutt-målet printes det ut til skjermen at bilen er fremme og har parkert.
+
+        }
+    }
+}
